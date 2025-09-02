e4b1c9c7fcb79c0c024f1fa937df8458e09eb170
Mohammad Reza Malakouti <malakouti@outlook.com>
2025-09-02 16:57:42 +0330

    new

--- BeauProject.CRM.IoC/DependencyContainer.cs
+++ BeauProject.CRM.IoC/DependencyContainer.cs
@@ -1,51 +1,51 @@
 ﻿using BeauProject.Shared.Implements;
 using BeauProject.Shared.Interfaces;
-using Microsoft.Extensions.DependencyInjection;
+//using Microsoft.Extensions.DependencyInjection;
 using System.Text;
 
 namespace BeauProject.CRM.IoC
 {
     public static class DependencyContainer
     {
-        public static IServiceCollection RegisterServices(this IServiceCollection service,
-            IConfiguration configuration)
-        {
-            #region Application Layer
-            service.AddScoped<IJwtHandler, JwtHandler>();
-            //service.AddMediatR(Assembly.GetExecutingAssembly());
-            //service.AddAutoMapper(Assembly.GetExecutingAssembly());
-            service.AddMediatR(typeof(RegisterUserHandler).Assembly);
-            service.AddAutoMapper(typeof(AutoMapperProfiler).Assembly);
-            #endregion
+        //public static IServiceCollection RegisterServices(this IServiceCollection service,
+        //    IConfiguration configuration)
+        //{
+        //    #region Application Layer
+        //    service.AddScoped<IJwtHandler, JwtHandler>();
+        //    //service.AddMediatR(Assembly.GetExecutingAssembly());
+        //    //service.AddAutoMapper(Assembly.GetExecutingAssembly());
+        //    service.AddMediatR(typeof(RegisterUserHandler).Assembly);
+        //    service.AddAutoMapper(typeof(AutoMapperProfiler).Assembly);
+        //    #endregion
 
-            #region Data Layer
-            service.AddScoped<IUserRepository, UserRepository>();
-            service.AddDbContext<IdentityContext>(option =>
-                option.UseSqlServer(configuration.GetConnectionString("BeauManager")));
-            #endregion
+        //    #region Data Layer
+        //    service.AddScoped<IUserRepository, UserRepository>();
+        //    service.AddDbContext<IdentityContext>(option =>
+        //        option.UseSqlServer(configuration.GetConnectionString("BeauManager")));
+        //    #endregion
 
-            #region Auth
-            service.AddAuthentication(option =>
-            {
-                option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-            }).AddJwtBearer(opt =>
-            {
-                opt.TokenValidationParameters = new TokenValidationParameters
-                {
-                    ValidateLifetime = true,
-                    ValidateIssuer = false,
-                    ValidateAudience = false,
-                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["jwt:key"]))
-                };
-            });
-            #endregion
+        //    #region Auth
+        //    service.AddAuthentication(option =>
+        //    {
+        //        option.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+        //        option.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+        //    }).AddJwtBearer(opt =>
+        //    {
+        //        opt.TokenValidationParameters = new TokenValidationParameters
+        //        {
+        //            ValidateLifetime = true,
+        //            ValidateIssuer = false,
+        //            ValidateAudience = false,
+        //            IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["jwt:key"]))
+        //        };
+        //    });
+        //    #endregion
 
-            #region Shared Layer
-            service.AddScoped<IEncrypter, Encrypter>();
-            #endregion
+        //    #region Shared Layer
+        //    service.AddScoped<IEncrypter, Encrypter>();
+        //    #endregion
 
-            return service;
-        }
+        //    return service;
+        //}
     }
 }
