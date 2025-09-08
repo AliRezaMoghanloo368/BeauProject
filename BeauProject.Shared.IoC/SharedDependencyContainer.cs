680ae85eb71c9b9770a8a6df9c91dced94cfb4b0
alireza moghanloo <ar.moghanloo368@gmail.com>
2025-09-08 19:36:34 +0330

    new

---
+++ BeauProject.Shared.IoC/SharedDependencyContainer.cs
@@ -0,0 +1,31 @@
+﻿using BeauProject.Shared.Data.Context;
+using BeauProject.Shared.Data.Repositories;
+using BeauProject.Shared.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BeauProject.Shared.IoC
+{
+    public static class SharedDependencyContainer
+    {
+        public static IServiceCollection RegisterSharedServices(this IServiceCollection service,
+            IConfiguration configuration)
+        {
+            #region Application Layer
+            //service.AddMediatR(Assembly.GetExecutingAssembly());
+            //service.AddAutoMapper(Assembly.GetExecutingAssembly());
+            //service.AddMediatR(typeof(RegisterUserHandler).Assembly);
+            //service.AddAutoMapper(typeof(AutoMapperProfiler).Assembly);
+            #endregion
+
+            #region Data Layer
+            service.AddScoped<IFilesRepository, FilesRepository>();
+            service.AddDbContext<SharedContext>(option =>
+                option.UseSqlServer(configuration.GetConnectionString("BeauManager")));
+            #endregion
+
+            return service;
+        }
+    }
+}
