e8f750d6f89559ed1dc8beac9358414f99621dfc
TARA3 <TARA3@TARA3.com>
2025-09-14 10:32:27 +0330

    Define Table

---
+++ BeauProject.Shared.Domain/Models/GroupProducts.cs
@@ -0,0 +1,11 @@
+﻿namespace BeauProject.Shared.Domain.Models
+{
+    public class GroupProducts
+    {
+        public GroupProducts()
+        {
+        }
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
---
+++ BeauProject.Shared.Domain/Models/Products.cs
@@ -0,0 +1,14 @@
+﻿namespace BeauProject.Shared.Domain.Models
+{
+    public class Products
+    {
+        public Products()
+        {
+        }
+        public int Id { get; set; }
+        public int Code { get; set; }
+        public string Name { get; set; }
+        public decimal Price { get; set; }
+        public int GroupProducts { get; set; }
+    }
+}
